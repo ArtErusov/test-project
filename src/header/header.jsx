560ba6c854e7e232cad837544cc9b0ea1621386f
Artem Erusov <erusovav@gmail.com>
2023-12-03 15:51:17 +0300

    add drop-down menu

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,14 +1,25 @@
 import styles from './Header.module.scss';
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [isOpen, setOpen] = useState(false); // состояние всплывающего меню
+
  return(<div className={styles.header}>
  <div className={styles.container}>
         <div className={styles.header__flex}>
             <nav>Мой учебный проект</nav>
             <nav>
                 <ul className={styles.header__flex__right}>
-                    <li><Link to="http://localhost:3000/independentone">Самостоятельная работа</Link></li>
+                    <li>
+                        <button onClick={() => setOpen(!isOpen)}>Самостоятельная работа</button>
+                        <ul className={styles.drop + ' ' +  `${isOpen ? styles.drop_active : ""}`}>
+                             {/* <= логика всплывающего меню */}
+                            <li><a href="">todo list</a></li>
+                            <li><a href="">add user</a></li>
+                        </ul>
+                    </li>
                     <li><Link to="http://localhost:3000/deliveryprototype">Доставка суши</Link></li>
                 </ul>
             </nav>
